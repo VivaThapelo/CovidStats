090a54f8e8e47cf85382d752b6d3655a93628e3d
Thapelo Radebe <thapelo@iteampay.co.za>
2020-08-29 22:35:57 +0200

    Initial commit

---
+++ app/src/main/java/com/thapelo/covidstats/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.thapelo.covidstats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.thapelo.covidstats.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
